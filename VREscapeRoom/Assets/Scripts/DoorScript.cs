cb5536f4a32f0501966f104cb3bef734d9687539
proteindwarf <80240092+proteindwarf@users.noreply.github.com>
2021-05-20 00:53:25 +0900

    방구조(완성), 문 키로 여는거 (미완성)

---
+++ VREscapeRoom/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public static bool doorKey;
+    public bool open;
+    public bool close;
+    public bool inTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (close)
+            {
+                if (doorKey)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        open = true;
+                        close = false;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    close = true;
+                    open = false;
+                }
+            }
+        }
+
+        if (open)
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 90.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            if (open)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "CLOSE");
+            }
+            else
+            {
+                if (doorKey)
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "DOOR OPEN");
+                }
+                else
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "Need a key!");
+                }
+            }
+        }
+    }
+}
