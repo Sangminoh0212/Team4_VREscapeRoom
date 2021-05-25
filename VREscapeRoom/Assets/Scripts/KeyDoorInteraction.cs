9b9aea916469f7c09024eec05688be748da29b62
Sangminoh0212 <ohsm1226@naver.com>
2021-05-25 14:38:58 +0900

    씬 0525추가, 문에 socket interactor 추가함

--- VREscapeRoom/Assets/Scripts/KeyDoorInteraction.cs
+++ VREscapeRoom/Assets/Scripts/KeyDoorInteraction.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class KeyDoorInteraction : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    public bool dooropen = false;
+
+    // 일단은 닿기만 하면 사라지게 만듬. 나중에 닿으면 true -> 문 door operator 가능하게 만들 예정
+    public void lockeddoor()
     {
-        if (collision.gameObject.CompareTag("Key"))
-        {
-            Destroy(transform.gameObject);
-        }
+        Destroy(transform.gameObject);
     }
+
 }
